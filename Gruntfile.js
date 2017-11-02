e93d769ded7c53711773c634b25d821dba148fb2
John Dulaney <john.s.dulaney1@gmail.com>
2017-11-02 10:46:18 -0500

    grunt 8

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,9 +31,11 @@ module.exports = function(grunt) {
         // Load the plugin that provides the "uglify" task.
         grunt.loadNpmTasks('grunt-contrib-uglify');
         grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.loadNpmTasks('eslint-grunt');
+        
         
     
         // Default task(s).
-        grunt.registerTask('default', ['uglify', 'watch']);
+        grunt.registerTask('default', ['uglify', 'watch', 'eslint']);
     
     };
